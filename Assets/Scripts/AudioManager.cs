85abb8da7a54c63e63a3976b841032ce637cdef8
JuanjoAlcover11 <61268085+JuanjoAlcover11@users.noreply.github.com>
2022-03-18 22:38:38 +0100

    a

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,19 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
+    //Array para meter todos los clips de audio
     public AudioClip[] clips;
-   // private int song;
+    //Variable de tipo entero que representa la cancion que esta sonando
+    private int song;
+    //AudioSource para reproducir los audios
+    private AudioSource AudioSource;
+
+    //Array para meter todos los nombres de las canciones
+    public string[] songTitles;
+    //Texto para el titulo de la cancion
+    public TextMeshProUGUI songText;
+
     void Start()
     {
-        
+        AudioSource = GetComponent<AudioSource>();
+        //Reproduir la primera cancion
+        AudioSource.PlayOneShot(clips[song]);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //Hacemos que aparezca en pantalla el nombre de la cancion que esta sonando
+        songText.text = songTitles[song]; 
+    }
+
+    public void PauseButtom()
+    {
+        //Hacemos pausar el audio al pulsar el boton
+        AudioSource.Pause();
+    }
+
+    public void PlayButtom()
+    {
+        //Hacemos reproducir el audio al pulsar el boton
+        AudioSource.UnPause();
+    }
+
+    public void NextButtom()
+    {
+        //Aumentamos en 1 el valor de la variable para pasar a la siguiente cancion
+        song++;
+        //Hacemos que si pasamos de la ultima cancion se reproduzca la primera
+        if (song >= clips.Length)
+        {
+            song = 0;
+        }
+        //Paramos la cancion anterior
+        AudioSource.Stop();
+        //Hacemos que se reproduzca la nueva
+        AudioSource.PlayOneShot(clips[song]);
+    }
+
+    public void PreviousButtom()
+    {
+        //Reducimos en 1 el valor de la variable para pasar a la cancion anterior
+        song--;
+        //Hacemos que si pasamos de la primera cancion se reproduzca la ultima
+        if (song < 0)
+        {
+            song = clips.Length - 1;
+        }
+        //Paramos la cancion anterior
+        AudioSource.Stop();
+        //Hacemos que se reproduzca la nueva
+        AudioSource.PlayOneShot(clips[song]);
+    }
+
+    public void ShuffleButtom()
+    {
+        //Paramos la cancion anterior
+        AudioSource.Stop();
+        //Hacemos que el valor entero sea aleatorio
+        song = Random.Range(0, clips.Length);
+        //Hacemos que se reproduzca la nueva
+        AudioSource.PlayOneShot(clips[song]);
+
     }
 }
